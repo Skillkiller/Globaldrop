42130e04972a0ee08d06a30f1fd4671812245f5e
Skillkiller <seblsebastian@aol.de>
2025-01-13 23:55:33 +0100

    Expose openDialog function to App

--- gdclient/src/App.tsx
+++ gdclient/src/App.tsx
@@ -29,155 +29,157 @@ function App() {
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const dialogRef = useRef<HTMLButtonElement | null>();
+    const [openDialogFunction, setOpenDialogFunction] = useState<
+      (() => void) | null
+    >(null);
 
-  const openFileProgressDialog = () => {
-    console.log("Öffne Dialog");
-    console.log(dialogRef.current);
-    if (dialogRef.current) dialogRef.current.click();
-  };
-
-  useEffect(() => {
-    if (!peerRef.current) {
-      console.log("Initializing Peer");
-      peerRef.current = new Peer({
-        host: import.meta.env.VITE_BACKEND_HOST,
-        port: import.meta.env.VITE_BACKEND_PORT,
-        path: import.meta.env.VITE_BACKEND_PEER_PATH,
-        secure: import.meta.env.VITE_PEER_SECURE === "true",
-      }); // Peer wird nur einmal erstellt
-    }
-
-    const getRoomData = async () => {
-      const backendHost = import.meta.env.VITE_BACKEND_HOST;
-      const backendPort = import.meta.env.VITE_BACKEND_PORT;
-      const backendSecure = import.meta.env.VITE_BACKEND_SECURE === "true";
-      const backendRoomPath = import.meta.env.VITE_BACKEND_ROOM_PATH;
-
-      const protocol = backendSecure ? "https" : "http";
-      const url = `${protocol}://${backendHost}:${backendPort}/${backendRoomPath}`;
-
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error("Fehler beim Abrufen der Room Daten!");
-        }
-        const data: RoomData = await response.json();
-
-        data.clients.forEach((ident) => {
-          const conn = peerRef.current?.connect(ident, {
-            reliable: true,
-          });
-          addDataConnectionListener(
-            setPeers,
-            conn!,
-            setFileProgressList,
-            openFileProgressDialog
-          );
-        });
-      } catch (error) {
-        console.error("Fehler bei der Anfrage:", error);
-      }
+    const openFileProgressDialog = () => {
+      if (openDialogFunction) openDialogFunction();
     };
 
-    // Beispiel: Event-Listener registrieren
-    peerRef.current.on("open", (id) => {
-      console.log("Peer ID:", id);
-      setIdentNumber(id);
-      getRoomData();
-    });
+    useEffect(() => {
+      if (!peerRef.current) {
+        console.log("Initializing Peer");
+        peerRef.current = new Peer({
+          host: import.meta.env.VITE_BACKEND_HOST,
+          port: import.meta.env.VITE_BACKEND_PORT,
+          path: import.meta.env.VITE_BACKEND_PEER_PATH,
+          secure: import.meta.env.VITE_PEER_SECURE === "true",
+        }); // Peer wird nur einmal erstellt
+      }
 
-    peerRef.current.on("connection", (dataConnection: DataConnection) => {
-      addDataConnectionListener(
-        setPeers,
-        dataConnection,
+      const getRoomData = async () => {
+        const backendHost = import.meta.env.VITE_BACKEND_HOST;
+        const backendPort = import.meta.env.VITE_BACKEND_PORT;
+        const backendSecure = import.meta.env.VITE_BACKEND_SECURE === "true";
+        const backendRoomPath = import.meta.env.VITE_BACKEND_ROOM_PATH;
+
+        const protocol = backendSecure ? "https" : "http";
+        const url = `${protocol}://${backendHost}:${backendPort}/${backendRoomPath}`;
+
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error("Fehler beim Abrufen der Room Daten!");
+          }
+          const data: RoomData = await response.json();
+
+          data.clients.forEach((ident) => {
+            const conn = peerRef.current?.connect(ident, {
+              reliable: true,
+            });
+            addDataConnectionListener(
+              setPeers,
+              conn!,
+              setFileProgressList,
+              openFileProgressDialog
+            );
+          });
+        } catch (error) {
+          console.error("Fehler bei der Anfrage:", error);
+        }
+      };
+
+      // Beispiel: Event-Listener registrieren
+      peerRef.current.on("open", (id) => {
+        console.log("Peer ID:", id);
+        setIdentNumber(id);
+        getRoomData();
+      });
+
+      peerRef.current.on("connection", (dataConnection: DataConnection) => {
+        addDataConnectionListener(
+          setPeers,
+          dataConnection,
+          setFileProgressList,
+          openFileProgressDialog
+        );
+      });
+
+      // Cleanup: Peer-Instanz zerstören, aber nur beim Unmount der Komponente
+      return () => {
+        if (peerRef.current) {
+          peerRef.current.destroy();
+          peerRef.current = undefined;
+          console.log("Peer destroyed");
+        }
+      };
+    }, []);
+
+    function onFilesSelected(event: ChangeEvent<HTMLInputElement>): void {
+      startSendingFiles(
+        event,
+        peerRef,
         setFileProgressList,
         openFileProgressDialog
       );
-    });
+    }
 
-    // Cleanup: Peer-Instanz zerstören, aber nur beim Unmount der Komponente
-    return () => {
-      if (peerRef.current) {
-        peerRef.current.destroy();
-        peerRef.current = undefined;
-        console.log("Peer destroyed");
-      }
-    };
-  }, []);
-
-  function onFilesSelected(event: ChangeEvent<HTMLInputElement>): void {
-    startSendingFiles(
-      event,
-      peerRef,
-      setFileProgressList,
-      openFileProgressDialog
-    );
-  }
-
-  return (
-    <ThemeProvider storageKey="vite-ui-theme">
-      <div className="flex flex-col lg:flex-row min-h-screen">
-        {/* Sidebar */}
-        <div className="w-full max-w-lg p-4 flex flex-col space-y-4">
-          <Card>
-            <CardHeader>
-              <div className="flex flex-row items-center gap-4">
-                <div className="flex aspect-square items-center justify-center rounded-lg size-8 bg-primary text-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+    return (
+      <ThemeProvider storageKey="vite-ui-theme">
+        <div className="flex flex-col lg:flex-row min-h-screen">
+          {/* Sidebar */}
+          <div className="w-full max-w-lg p-4 flex flex-col space-y-4">
+            <Card>
+              <CardHeader>
+                <div className="flex flex-row items-center gap-4">
+                  <div className="flex aspect-square items-center justify-center rounded-lg size-8 bg-primary text-primary-foreground">
+                    <GalleryVerticalEnd className="size-4" />
+                  </div>
+                  <p className="text-3xl">GlobalDrop</p>
                 </div>
-                <p className="text-3xl">GlobalDrop</p>
-              </div>
-              <Separator></Separator>
-            </CardHeader>
-            <CardContent className="flex w-full items-center justify-center ">
-              <IdentShow ident={identNumber}></IdentShow>
-            </CardContent>
-          </Card>
-          <ConnectionCard
-            peerRef={peerRef}
-            peers={peers}
-            setPeers={setPeers}
-            inputElementRef={inputRef}
-            setFileProgressList={setFileProgressList}
-            openFileProgressDialog={openFileProgressDialog}
-          ></ConnectionCard>
-          <div className="flex-grow">
-            <ProgressDialog
-              fileProgressList={fileProgressList}
-              ref={dialogRef}
-            ></ProgressDialog>
+                <Separator></Separator>
+              </CardHeader>
+              <CardContent className="flex w-full items-center justify-center ">
+                <IdentShow ident={identNumber}></IdentShow>
+              </CardContent>
+            </Card>
+            <ConnectionCard
+              peerRef={peerRef}
+              peers={peers}
+              setPeers={setPeers}
+              inputElementRef={inputRef}
+              setFileProgressList={setFileProgressList}
+              openFileProgressDialog={openFileProgressDialog}
+            ></ConnectionCard>
+            <div className="flex-grow">
+              <ProgressDialog
+                fileProgressList={fileProgressList}
+                callbackOpenDialog={(openDialog) =>
+                  setOpenDialogFunction(() => openDialog)
+                }
+              ></ProgressDialog>
+            </div>
           </div>
-        </div>
 
-        {/* Main Content */}
-        <div className="flex-grow p-4">
-          <PeerDisplay peers={peers} inputElementRef={inputRef} />
-        </div>
+          {/* Main Content */}
+          <div className="flex-grow p-4">
+            <PeerDisplay peers={peers} inputElementRef={inputRef} />
+          </div>
 
-        <input
-          type="file"
-          multiple
-          style={{ display: "none" }}
-          ref={inputRef}
-          onChange={onFilesSelected}
-        ></input>
-      </div>
-      <Card className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50 w-auto max-w-[90%]">
-        <CardContent className="p-2 text-center text-sm">
-          <p>
-            Globaldrop v{APP_VERSION} -{" "}
-            <a
-              href="https://github.com/Skillkiller/Globaldrop"
-              className="underline"
-            >
-              GitHub
-            </a>
-          </p>
-        </CardContent>
-      </Card>
-    </ThemeProvider>
-  );
+          <input
+            type="file"
+            multiple
+            style={{ display: "none" }}
+            ref={inputRef}
+            onChange={onFilesSelected}
+          ></input>
+        </div>
+        <Card className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50 w-auto max-w-[90%]">
+          <CardContent className="p-2 text-center text-sm">
+            <p>
+              Globaldrop v{APP_VERSION} -{" "}
+              <a
+                href="https://github.com/Skillkiller/Globaldrop"
+                className="underline"
+              >
+                GitHub
+              </a>
+            </p>
+          </CardContent>
+        </Card>
+      </ThemeProvider>
+    );
 }
 
 export default App;
--- gdclient/src/components/dialog/progress-dialog.tsx
+++ gdclient/src/components/dialog/progress-dialog.tsx
@@ -10,7 +10,7 @@ import {
 import { Progress } from "../ui/progress";
 import { ChevronsLeftRightEllipsis } from "lucide-react";
 import { FileMetadata } from "@/lib/network";
-import { forwardRef, useRef, useImperativeHandle } from "react";
+import { useEffect, useRef } from "react";
 import React from "react";
 
 export interface FileProgress extends FileMetadata {
@@ -19,74 +19,88 @@ export interface FileProgress extends FileMetadata {
   buffer: ArrayBuffer[];
 }
 
-export const ProgressDialog = forwardRef(
-  ({ fileProgressList }: { fileProgressList: FileProgress[] }, ref) => {
-    const firstButtonRef = useRef<HTMLButtonElement | null>(null);
+export const ProgressDialog = ({
+  fileProgressList,
+  callbackOpenDialog,
+}: {
+  fileProgressList: FileProgress[];
+  callbackOpenDialog: (openDialog: () => void) => void;
+}) => {
+  const firstButtonRef = useRef<HTMLButtonElement | null>(null);
 
-    useImperativeHandle(ref, () => firstButtonRef.current);
+  let dialogOpen = false;
 
-    function saveFile(fileProgress: FileProgress) {
-      console.log(fileProgress);
-      const blob = new Blob(fileProgress.buffer, {
-        type: fileProgress.mime,
-      });
-      const downloadUrl = URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = downloadUrl;
-      link.download = fileProgress.name || "download";
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      URL.revokeObjectURL(downloadUrl);
+  function openDialog(): void {
+    if (!dialogOpen && firstButtonRef.current) {
+      firstButtonRef.current.click();
     }
-
-    return (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline" className="w-full" ref={firstButtonRef}>
-            <ChevronsLeftRightEllipsis />
-            Dateien
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Fortschritt</DialogTitle>
-          </DialogHeader>
-          {fileProgressList.length === 0 && (
-            <h1>Es wurden bisher keine Daten übertragen</h1>
-          )}
-          <div className="grid grid-cols-2 gap-4 justify-between items-center ">
-            {fileProgressList.map((f) => (
-              <React.Fragment key={f.id}>
-                <Button
-                  key={f.id + "-dl-button"}
-                  variant={
-                    f.chunksReceived !== f.totalChunks ? "outline" : "default"
-                  }
-                  disabled={
-                    f.chunksReceived !== f.totalChunks || f.mode !== "Receiver"
-                  }
-                  onClick={(e) => {
-                    e.preventDefault();
-                    saveFile(f);
-                  }}
-                >
-                  <p className="text-ellipsis overflow-hidden">{f.name}</p>
-                </Button>
-                <Progress
-                  key={f.id + "-status"}
-                  value={(f.chunksReceived / f.totalChunks) * 100}
-                  className="h-1/3"
-                />
-              </React.Fragment>
-            ))}
-          </div>
-          <DialogFooter></DialogFooter>
-        </DialogContent>
-      </Dialog>
-    );
   }
-);
+
+  useEffect(() => {
+    callbackOpenDialog(openDialog);
+  }, [callbackOpenDialog]);
+
+  function saveFile(fileProgress: FileProgress) {
+    console.log(fileProgress);
+    const blob = new Blob(fileProgress.buffer, {
+      type: fileProgress.mime,
+    });
+    const downloadUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = downloadUrl;
+    link.download = fileProgress.name || "download";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(downloadUrl);
+  }
+
+  return (
+    <Dialog onOpenChange={(open) => (dialogOpen = open)}>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="w-full" ref={firstButtonRef}>
+          <ChevronsLeftRightEllipsis />
+          Dateien
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Fortschritt</DialogTitle>
+        </DialogHeader>
+        {fileProgressList.length === 0 && (
+          <h1>Es wurden bisher keine Daten übertragen</h1>
+        )}
+        <div className="grid grid-cols-2 gap-4 justify-between items-center ">
+          {fileProgressList.map((f) => (
+            <React.Fragment key={f.id}>
+              <Button
+                key={f.id + "-dl-button"}
+                variant={
+                  f.chunksReceived !== f.totalChunks ? "outline" : "default"
+                }
+                disabled={
+                  f.chunksReceived !== f.totalChunks || f.mode !== "Receiver"
+                }
+                onClick={(e) => {
+                  e.preventDefault();
+                  saveFile(f);
+                }}
+              >
+                <p className="text-ellipsis overflow-hidden">{f.name}</p>
+              </Button>
+              <Progress
+                key={f.id + "-status"}
+                value={(f.chunksReceived / f.totalChunks) * 100}
+                className="h-1/3"
+              />
+            </React.Fragment>
+          ))}
+        </div>
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default ProgressDialog;
